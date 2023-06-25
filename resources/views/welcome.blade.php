d4d800a4b83c691933cb5387ee556e14c1796d1a
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2023-06-25 14:32:55 +0200

    structure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,10 +28,99 @@
         <img class="jumbo" src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="">
     </header>
     <main>
-
+        <div class="sub_cont">
+            <div class="card_cont">
+                <div class="card">
+                    <img src="" alt="card-img">
+                    <h4></h4>
+                </div>
+                <div class="card">
+                    <img src="" alt="card-img">
+                    <h4></h4>
+                </div>
+                <div class="card">
+                    <img src="" alt="card-img">
+                    <h4></h4>
+                </div>
+                <div class="card">
+                    <img src="" alt="card-img">
+                    <h4></h4>
+                </div>
+                <div class="card">
+                    <img src="" alt="card-img">
+                    <h4></h4>
+                </div>
+                <div class="card">
+                    <img src="" alt="card-img">
+                    <h4></h4>
+                </div>
+                <div class="card">
+                    <img src="" alt="card-img">
+                    <h4></h4>
+                </div>
+            </div>
+            <button>LOAD MORE</button>
+        </div>
     </main>
     <footer>
-
+        <div class="top-foot">
+            <div class="max-w">
+                <div class="features-ft"
+                v-for="obj in ftarr" 
+                >
+                    <img src="" alt="">
+                    <span>titlte</span>
+                </div>
+            </div>
+        </div>
+        <div class="main-foot">
+            <div class="max-w">
+                <div class="sub-cont">
+                    <div >
+                        <ul>
+                            <li>DC COMICS</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                        </ul>
+                        <ul>
+                            <li>DC COMICS</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                        </ul>
+                    </div>
+                    <div >
+                        <ul>
+                            <li>DC COMICS</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                        </ul>
+                    </div>
+                    <div >
+                        <ul>
+                            <li>DC COMICS</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                            <li>Comics</li>
+                        </ul>
+                    </div>
+                </div>
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+            </div>
+        </div>
+        <div class="bottom-foot">
+            <div class="max-w"></div>
+        </div>
     </footer>
 </body>
 </html>
